ffebe4fba0b51266f4a5dcfb3e0bc715a6404c15
Ouchdex <3393435537@qq.com>
2021-10-05 00:06:46 +0800

    展开操作符

--- 03 语言基础/19 展开操作符.js
+++ 03 语言基础/19 展开操作符.js
@@ -2,8 +2,13 @@ const add = (x, y, z) => x + y + z;
 const arr = [1, 2, 3];
 console.log(add(...arr));               // 6
 
-console.log([arr, 4, 5, 6]);            // [ [ 1, 2, 3 ], 4, 5, 6 ]
-console.log([...arr, 4, 5, 6]);         // [ 1, 2, 3, 4, 5, 6 ]
+const arr1 = [arr, 4, 5, 6];
+const arr2 = [...arr, 4, 5, 6];
+console.log(arr1);                        // [ [ 1, 2, 3 ], 4, 5, 6 ]
+console.log(arr2);                        // [ 1, 2, 3, 4, 5, 6 ]
+arr[0] = 0;
+console.log(arr1);                        // [ [ 0, 2, 3 ], 4, 5, 6 ]
+console.log(arr2);                        // [ 1, 2, 3, 4, 5, 6 ]
 
 const obj1 = {name: 'obj1'};
 const obj2 = {...obj1};
